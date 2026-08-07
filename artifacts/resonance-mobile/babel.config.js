1c75da8520a652584253c3c50a251d22ec1d1125
The-Nexus-system <abrahamson.nicholas@gmail.com>
2026-08-07 07:31:19 -0400

    Fix production publish: enable @babel/plugin-transform-class-static-block so three.js bundles in minified Metro builds

--- artifacts/resonance-mobile/babel.config.js
+++ artifacts/resonance-mobile/babel.config.js
@@ -2,5 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
+    // three.js (Phase 4 3-D scene) ships static class blocks, which the
+    // production (minified) Metro transform does not enable by default.
+    plugins: ['@babel/plugin-transform-class-static-block'],
   };
 };
