5eb343ff3c1cd1d8cd33d304c5e0ca88166e62b1
The-Nexus-system <abrahamson.nicholas@gmail.com>
2026-08-07 07:51:29 -0400

    Fix production bundling: pin @babel/plugin-transform-class-static-block to v7 (v8 dragged Babel 8 types into hoist, breaking worklets plugin) and scope it to three.js only

--- artifacts/resonance-mobile/babel.config.js
+++ artifacts/resonance-mobile/babel.config.js
@@ -4,6 +4,13 @@ module.exports = function (api) {
     presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
     // three.js (Phase 4 3-D scene) ships static class blocks, which the
     // production (minified) Metro transform does not enable by default.
-    plugins: ['@babel/plugin-transform-class-static-block'],
+    // Scoped to three.js only: applying it globally breaks the
+    // reanimated/worklets Babel plugin (NumericLiterals exception).
+    overrides: [
+      {
+        test: /node_modules[\\/](\.pnpm[\\/][^\\/]+[\\/]node_modules[\\/])?three[\\/]/,
+        plugins: ['@babel/plugin-transform-class-static-block'],
+      },
+    ],
   };
 };
